3bb192ec9faaeea4de23542fe2456bd476f268fb
junhyung8795 <junhyung8795@naver.com>
2025-01-15 22:43:52 +0900

    1월15일자 정적 중첩클래스 실험
    static변수는 해당 클래스의 모든 인스턴스들이 공유하는 변수인데, 정적 중첩 클래스는 아닌 것 같았다. LinkedList예제를 직접 구현하는 과정에서 정적 중첩 클래스로 Node가 있었는데 Node는 LinkedList의 인스턴스 변수에 묶여있지만 않았지 인스턴스마다 각각 Node를 가지고 있었고 이는 static변수와 다른 성질이라고 생각했기 때문이다. 이를 증명하기 위한 예제를 직접 만들어봤다.

---
+++ untitled/src/javaCodePractice/staticInnerPractice/StaticInner.java
@@ -0,0 +1,18 @@
+package javaCodePractice.staticInnerPractice;
+
+public class StaticInner {
+    public Node node = new Node();
+    private static class Node{
+        int data = 4;
+    }
+
+    public static int date2 = 4;
+
+
+
+    public void display() {
+        System.out.println("node.data = " + node.data);
+        System.out.println("node = " + node);
+        System.out.println("date2 = " + date2);
+    }
+}
---
+++ untitled/src/javaCodePractice/staticInnerPractice/StaticInnerMain.java
@@ -0,0 +1,14 @@
+package javaCodePractice.staticInnerPractice;
+
+public class StaticInnerMain {
+    public static void main(String[] args) {
+        StaticInner staticInner = new StaticInner();
+        StaticInner staticInner1 = new StaticInner();
+        staticInner.display();
+        staticInner1.display();
+        StaticInner.date2 = 5;
+        staticInner.display();
+        staticInner1.display();
+
+    }
+}

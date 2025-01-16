bc4db6fc8cdd47d5c37b765ef6441760239bfb3c
junhyung8795 <junhyung8795@naver.com>
2025-01-16 15:14:44 +0900

    1.16 해시 코드와 해시 인덱스의 정의
    해시 코드는 어떤 데이터를 대표하는 값. 해시 인덱스는 이 해시 코드로 연산한 결과 값. 배열의 해시 인덱스에 해당하는 곳에 데이터를 저장함. 보통 해시 인덱스는 해시 코드를 배열의 용량으로 나누지만 다른 연산이 들어가기도 한다.

--- untitled/src/javaCodePractice/set/StringHashMain.java
+++ untitled/src/javaCodePractice/set/StringHashMain.java
@@ -15,15 +15,29 @@ public class StringHashMain {
         System.out.println("hashCode('A') = " + hashCode("A"));
         System.out.println("hashCode('B') = " + hashCode("B"));
         System.out.println("hashCode(\"AB\") = " + hashCode("AB"));
+
+        //hashIndex
+        int hashCodeA = hashCode("A");
+        int hashIndex = hashIndex(hashCodeA);
+        System.out.println("hashIndex = " + hashIndex);
+
+        System.out.println("hashIndex(A) " + hashIndex(hashCode("A")));
+        System.out.println("hashIndex(B) " + hashIndex(hashCode("B")));
+        System.out.println("hashIndex(AB) " + hashIndex(hashCode("AB")));
+
     }
 
-    static int hashCode(String str ){
+    static int hashCode(String str){
         char[] charArray = str.toCharArray();//char를 이용하면 int로 캐스팅해서 쉽게 아
         int sum = 0;
         for (int i = 0; i < charArray.length; i++) {
             sum += (int) charArray[i];//각 문자들을 int로 캐스팅한 수 들을 더한 것을 해당 문자열의 해쉬코드라고 치자.
         }
         return sum;
+    }
+
+    static int hashIndex(int value){
+        return value % CAPACITY;
     }
 }
 

966b71e24c5f452bd6922c877f1ab9313a4beea9
junhyung8795 <junhyung8795@naver.com>
2025-01-20 14:13:39 +0900

    1.20 containsKey, remove메서드 실험

--- untitled/src/javaCodePractice/javaMap/MapMain2.java
+++ untitled/src/javaCodePractice/javaMap/MapMain2.java
@@ -14,5 +14,14 @@ public class MapMain2 {
         //같은 키에 밸류를 또 넣으면 어떻게 되나? --> 기존의 값이 교체된다.
         studentMap.put("John", 200);
         System.out.println("studentMap = " + studentMap);
+
+        //키를 가지고 있냐도 알 수 있다.
+        boolean b = studentMap.containsKey("John");
+        System.out.println("b = " + b);
+
+        //특정 학생의 값을 key로 찾아 삭제
+        studentMap.remove("John");
+        System.out.println("studentMap = " + studentMap);
+
     }
 }

01a5a7998902d2ddc83fc04eae036582ebd0d13f
junhyung8795 <junhyung8795@naver.com>
2025-01-20 15:48:16 +0900

    1.20 queue는 offer로 추가하고 peek은 stack과 달리 맨앞의 값이 보여지고 poll로 데이터를 꺼낼 수 있다.

--- untitled/src/javaCodePractice/dequeStackQueue/QueueMAin.java
+++ untitled/src/javaCodePractice/dequeStackQueue/QueueMAin.java
@@ -15,6 +15,19 @@ public class QueueMAin {
 //        Queue<Integer> queue = new LinkedList<>();//Queue의 구현체로 링크드 리스트도 있으니 사용해도 된다!
 //        다만 ArrayDeque가 성능이 더 좋다.
 
-        
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println("queue = " + queue);
+
+        //단순 조회 제거x
+        System.out.println("queue.peek() = " + queue.peek());//다음 나올 것이 뭔가 찝어보는게 peek! 맨 처음에 넣은 1이 나온다.
+
+        //데이터 꺼내기
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println("queue.poll() = " + queue.poll());
+        System.out.println("queue = " + queue);
+
     }
 }

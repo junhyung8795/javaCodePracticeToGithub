0358483e756d1d01f43bc4845467a02371422d14
junhyung8795 <junhyung8795@naver.com>
2025-01-21 20:17:07 +0900

    1.21 iterator의 대상이 될 간단한 자료 구조(내부에 배열을 가진 MyArr클래스) 생성

---
+++ untitled/src/javaCodePractice/iterator/MyArr.java
@@ -0,0 +1,14 @@
+package javaCodePractice.iterator;
+
+//iterator는 혼자 사용될 수 없다. 순회할 대상으로 간단한 자료 구조 구현
+//내부에 배열을 가지고 있는 단순한 자료 구조
+public class MyArr {
+
+    private int[] numbers;
+
+    public MyArr(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+
+}

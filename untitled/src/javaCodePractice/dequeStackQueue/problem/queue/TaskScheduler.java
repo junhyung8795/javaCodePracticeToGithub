3745cd9995ec26321397d544562333f1cc832b3e
junhyung8795 <junhyung8795@naver.com>
2025-01-21 16:38:33 +0900

    1.21 작업 예약 예제 완성
    Task라는 인터페이스를 받게 해서 3가지 taks를 모두 내부 queue에 offer로 담게하고 poll로 꺼내면 해당 인스턴스의 오버라이딩된 excute메서드를 실행한다.

---
+++ untitled/src/javaCodePractice/dequeStackQueue/problem/queue/TaskScheduler.java
@@ -0,0 +1,23 @@
+package javaCodePractice.dequeStackQueue.problem.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class TaskScheduler {
+    Queue<Task> queue = new ArrayDeque<>();
+    public void addTask(Task task) {
+        queue.offer(task);
+    }
+
+    public int getRemainingTasks() {
+        return queue.size();
+    }
+
+    public void processNextTask() {
+        Task poll = queue.poll();
+        if (poll != null){
+            poll.excute();
+        }
+
+    }
+}

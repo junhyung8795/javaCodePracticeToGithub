3946a32918a2d27cd620fa3a106ff1ae8a86b8f2
junhyung8795 <junhyung8795@naver.com>
2025-01-21 13:37:10 +0900

    1.21 문자열에 나오는 단어들의 개수를 Map에 저장하는 예제
    map.getOrDefault(word, 0)는 word키에 해당하는 value값이 없으면 0을 default로 반환한다는 코드다. word에 대한 value값이 없을 때 기본값을 설정하고 싶을 때 map.getOrDefault메서드 사용하자

---
+++ untitled/src/javaCodePractice/javaMap/WordFrequencyTest.java
@@ -0,0 +1,25 @@
+package javaCodePractice.javaMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyTest {
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+        String[] split = text.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : split){
+            if (map.containsKey(word)){
+                map.put(word, map.get(word)+1);
+            } else {
+                map.put(word, 1);
+            }
+//            Integer count = map.getOrDefault(word, 0);
+//            map.getOrDefault(word, 0)는 word키에 해당하는 value값이 없으면 0을 default로 반환한다는 코드다\
+//            count++;
+//            map.put(word, count); 이와같이 접근하는 방법도 있다. 
+
+        }
+
+        System.out.println(map);
+    }
+}

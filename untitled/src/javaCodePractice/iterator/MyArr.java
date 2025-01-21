55cea3cf2a312a604c24bb403f59aede26aa9e77
junhyung8795 <junhyung8795@naver.com>
2025-01-21 20:29:52 +0900

    1.21 이전 커밋 일부 수정
    Iterable한 객체에 대해 추가 설명

--- untitled/src/javaCodePractice/iterator/MyArr.java
+++ untitled/src/javaCodePractice/iterator/MyArr.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 //iterator는 혼자 사용될 수 없다. 순회할 대상으로 간단한 자료 구조 구현
 //내부에 배열을 가지고 있는 단순한 자료 구조
 //이 MyArr라는 자료 구조를 순회하게 만들려면 "순회할 수 있는"이라는 의미의 Iterable 인터페이스를 구현 해야한다.
-//이로써 Iterable한 객체가 된 것이다!
+//이로써 Iterable한 객체가 된 것이다! 나는 순회할 수 있어라는 의미가 담겨 있다.
 public class MyArr implements Iterable<Integer> {
 
     private int[] numbers;

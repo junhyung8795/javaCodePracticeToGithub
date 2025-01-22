bf46943b1c2d5b9f9f875a2f566c1d4604601c92
junhyung8795 <junhyung8795@naver.com>
2025-01-22 14:36:18 +0900

    1.22 String이나 Integer같은 자바가 제공하는 객체가 아닌 내가 만든 객체끼리 비교할 때 -->Comparable<MadeObject>활용>
    객체에게 자체적으로 비교하는 기준을 주고 싶다면. 예를 들어 여기선 name과 age가 있는데 나이로 정렬을 하고싶다면 이 클래스 가장 아래에 있는 compareTo메서드를 오버라이딩 하자.
    compareTo는 Comparable 인터페이스를 구현하여 오버라이딩 되는 메서드인데, Comparable의 제네릭 타입에 따라 compareTo의 매개변수 타입이 맞춰진다. <MadeObject>를 <Integer>로 바꾸면 compareTo(Integer o)이렇게 바뀌니 주의하자

---
+++ untitled/src/javaCodePractice/compare/MadeObject.java
@@ -0,0 +1,34 @@
+package javaCodePractice.compare;
+
+//나는 비교가 가능한 객체야라는 것을 티내 주어야 한다.
+//implements Comparable<MadeObject>
+public class MadeObject implements Comparable<MadeObject> {
+    private String name;
+    private int age;
+
+    public MadeObject(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "MadeObject{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(MadeObject o) {
+        return 0;
+    }
+}

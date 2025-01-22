b6de68c4a31637596b33b32ed2933eb266101cf0
junhyung8795 <junhyung8795@naver.com>
2025-01-22 17:43:28 +0900

    1.22 이전 커밋에 반영안된 것 추가

--- untitled/src/javaCodePractice/collectionUtil/OfMain.java
+++ untitled/src/javaCodePractice/collectionUtil/OfMain.java
@@ -1,2 +1,23 @@
-package javaCodePractice.collectionUtil;public class OfMain {
+package javaCodePractice.collectionUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OfMain {
+    public static void main(String[] args) {
+        //편리한 불변 컬렉션
+
+        List<Integer> list = List.of(1, 2, 3);//of로 생성하면 불변 List가 된다.
+   /*     list.add(1);//불변이라 값을 추가하거나
+        list.set(1, 10);//값을 바꾸는게 안된다.*/
+        Set<Integer> set = Set.of(1, 2, 3);//불변Set
+        Map<Integer, String> map = Map.of(1, "One", 2, "Two");//불변Map
+        System.out.println(list);
+        System.out.println(set);
+        System.out.println(map);
+        System.out.println("list.getClass() = " + list.getClass());
+        System.out.println("set.getClass() = " + set.getClass());
+        System.out.println("map.getClass() = " + map.getClass());
+    }
 }

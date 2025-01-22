aef568a0cef9a0713eb3c525b8cb96f2cfe77a12
junhyung8795 <junhyung8795@naver.com>
2025-01-22 14:24:03 +0900

    1.22 DescComparator 구현
    앞서 만든 AscComparator를 복사 붙여넣기 하는데 대신에 리턴 값을 기존에 사용하던
    return (o1 < o2) ? -1 : (o1 > o2) ? 1 : 0 에다가 -1을 곱해준다.
    이전의 오름차 기준에 -1을 곱하여 반대로 정렬하게 되는 것이다.

--- untitled/src/javaCodePractice/compare/SortMain2.java
+++ untitled/src/javaCodePractice/compare/SortMain2.java
@@ -7,9 +7,20 @@ public class  SortMain2 {
     public static void main(String[] args) {
         Integer[] arr = new Integer[]{3, 2, 1};
         System.out.println(Arrays.toString(arr));
-        System.out.println("Comparator 비교");
+        System.out.println("오름차 comparator 적용");
         Arrays.sort(arr, new AscComparator());
         System.out.println(Arrays.toString(arr));
+
+        System.out.println("내림자 comparator 적용");
+        Arrays.sort(arr, new DescComparator());
+        System.out.println(Arrays.toString(arr));
+    }
+    static class DescComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            System.out.println("o1 = " + o1 + " o2 = " + o2);
+            return ((o1 < o2) ? -1 : (o1 > o2) ? 1 : 0) * -1;
+        }
     }
 
     static class AscComparator implements Comparator<Integer> {
